f78d15a376d9d63bba3c60d45b5e3afe574176f9
Sam Billingham <sambillie.sb@gmail.com>
2018-02-13 21:07:10 +0000

    load latest photo feed into grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
-var grid = document.querySelector('.masonry');
-var msnry = new Masonry( grid, {
-  itemSelector: '.item',
-  columnWidth: '.item',
-  gutter: 24
+
+let query = { 'tag': 'photofeed', 'limit': 20 }
+let converter = new showdown.Converter()
+
+steem.api.getDiscussionsByCreated(query, (err, result) => {
+    if (err === null) {
+        displayImages(result)
+    } else {
+        console.log(err);
+    }
 });
+
+function displayImages(result){
+  for (let i = 0; i < result.length ; i++) {
+      let post = result[i];
+      let json = JSON.parse(post.json_metadata)
+      let placeholder = document.createElement('div');
+      placeholder.innerHTML = converter.makeHtml(post.body);
+      let image = placeholder.querySelector('img') ;
+      image = image ? image.src : json.image[0]
+
+      $('.gallery').append(`
+        <div class="item"><img class="item__image hidden" src="${image}"></div>
+        `)
+  }
+  checkImages()
+}
+
+function checkImages(){
+  let images = $('img.item__image');
+  let loaded = 0;
+
+  images.on('load',() => {
+    loaded++;
+    if (loaded == images.length )
+        initMasonry(images)
+  });
+}
+
+function initMasonry(images){
+  images.removeClass('hidden')
+  let msnry = new Masonry( '.gallery', {
+    itemSelector: '.item',
+    columnWidth: '.item',
+    gutter: 24
+  });
+}
