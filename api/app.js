1d496dd7f134cfd2e5d6360ff3eaa2daf2d5c538
Sam Billingham <sambillie.sb@gmail.com>
2018-04-23 17:43:22 +0100

    setup api endpoint for photogaphers

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+let db = require('./modules/db')
 
 var app = express();
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+let db = require('../modules/db')
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('index')
+  db.get().db('photofeed').collection('photographers').find().toArray( (error, result) => {
+    console.log('result', result)
+    if(error || result == null) { console.log(error) }
+    else { res.json({result: result}) }
+  });
 });
 
 module.exports = router;
