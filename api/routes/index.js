a2bb5cfc864cd5cf33c81d9aaed94cafda274853
Sam Billingham <sambillie.sb@gmail.com>
2018-05-10 17:13:29 +0100

    restructure - move pages to backend and add routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,6 +5,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index')
+})
+
+router.get('/photographers', function(req, res, next) {
+  res.render('photographers')
+})
+
+router.get('/@username', function(req, res, next) {
+  let username = req.params.username
+  res.render('profile',{username})
+})
+
+/* GET photographers. */
+router.get('/photographers.json', function(req, res, next) {
   console.log('index')
   db.get().db('photofeed').collection('photographers').find().toArray( (error, result) => {
     console.log('result', result)
