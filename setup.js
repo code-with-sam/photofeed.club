13ef57d695b6687dab15d393db5bd82603f54d56
Sam Billingham <sambillie.sb@gmail.com>
2018-05-10 23:32:59 +0100

    update - setup script read/write with db

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 const steem = require('steem')
 const db = require('./modules/db')
 const _ = require('lodash')
-const INTERVAL = 1000 * 60 * 60
+const INTERVAL = 1000 * 60 * 60 * 1 // 1 Hour
 
 let config = require('./config');
 const databaseURL = `mongodb://${config.db.user}:${config.db.password}@ds119150.mlab.com:19150/photofeed2`;
@@ -19,39 +19,38 @@ db.connect(databaseURL, (err) => {
      process.exit(1)
    } else {
      console.log('Connected to photofeed Datbase...')
+
+     setInterval(updateDatabase, INTERVAL)
    }
  })
 
-function getPhotofeedPosts(username){
-  return new Promise(function(resolve, reject) {
-    let query = { 'tag':  username, 'limit': 100 }
-    steem.api.getDiscussionsByBlog(query, (err, result) => {
-      result = result.filter(post => {
-        let tags = JSON.parse(post.json_metadata).tags
-        if( tags.indexOf('photofeed') > -1 || post.parent_permlink === 'photofeed') return post
-      })
-      resolve(result)
-    })
-  });
+
+function updateDatabase(){
+  storeLatestPhotoFeedPosts().then(data => updateFeaturedCount())
+}
+
+function updateFeaturedCount() {
+  getFeaturedPhotographerTotals().then(data => {
+    let usernames = data.map(post => post.author)
+    let numberOfFeaturedPosts = _.countBy(usernames);
+    let uniqueAuthors = [...new Set(usernames)]
+
+    processAllAuthorData(uniqueAuthors, numberOfFeaturedPosts)
+    .then(data => storeFeaturedCount(data))
+  })
 }
 
 function getPhotoFeedBlogEntries(){
-  return new Promise(function(resolve, reject) {
-    let a = steem.api.getBlogEntriesAsync('photofeed', 1000, 500)
-    let b = steem.api.getBlogEntriesAsync('photofeed', 500, 500)
-    Promise.all([a,b]).then(data => resolve(data))
-  });
+  return steem.api.getBlogEntriesAsync('photofeed', 10000, 20)
 }
 
 function processAllAuthorData(uniqueAuthors, numberOfFeaturedPosts){
   return new Promise(function(resolve, reject) {
     steem.api.getAccountsAsync(uniqueAuthors).then(data => {
-      console.log('RAW STEEM ACCOUNT DATA: ', data)
       data = data.map(user => {
         let json;
-        try {
-          json = JSON.parse(user.json_metadata).profile
-        } catch(e){console.log(e)}
+        try { json = JSON.parse(user.json_metadata).profile }
+        catch(e){ console.log(e) }
 
         return {
           username: user.name,
@@ -59,51 +58,21 @@ function processAllAuthorData(uniqueAuthors, numberOfFeaturedPosts){
           featured: numberOfFeaturedPosts[user.name]
         }
       })
-      console.log(data)
       resolve(data)
     })
   });
 }
 
-function storeInitialData(data){
-  console.log('DATA from process', data)
+function storeFeaturedCount(data){
   for (var i = 0; i < data.length; i++) {
-    db.get().db('photofeed2').collection('photographers').insert(data[i], (error, response) => {
+    db.get().db('photofeed2').collection('photographers').update( { username: data.username }, data[i], { upsert: true },(error, response) => {
       if(error || response == null) { console.log(error) }
-      else { console.log(response) }
     })
   }
+  console.log('Featured Count Upated')
 }
 
-getPhotoFeedBlogTotals().then(data => {
-  // let uniqueBlogEntries = _.unionBy(data[0], data[1], 'permlink');
-  console.log('Unique Photofeed Blog Entries: ', uniqueBlogEntries)
-
-  let usernames = data.map(post => post.author)
-  let numberOfFeaturedPosts = _.countBy(usernames);
-  console.log('Author feature count: ', numberOfFeaturedPosts)
-
-  let uniqueAuthors = [...new Set(usernames)]
-  console.log('Unique Authors: ', uniqueAuthors)
-
-  // let authorPhotofeedPosts = uniqueAuthors.map(author => getPhotofeedPosts(author))
-  // console.log('Authors Photofeed Posts Promises: ', authorPhotofeedPosts)
-  //
-  // Promise.all(authorPhotofeedPosts).then(data => {
-  //   let photofeedPosts = data.map( (posts, i) => {
-  //     return { author: uniqueAuthors[i], count: posts.length }
-  //   })
-  //   console.log('Authors Photodeed Posts Results: ', photofeedPosts)
-  //
-    processAllAuthorData(uniqueAuthors, numberOfFeaturedPosts)
-      .then(data => storeInitialData(data))
-  // })
-
-})
-
-
-
-function getPhotoFeedBlogTotals() {
+function getFeaturedPhotographerTotals() {
   return new Promise(function(resolve, reject) {
     db.get().db('photofeed2').collection('posts').find().toArray((error, response) => {
       if(error || response == null) console.log(error)
@@ -112,23 +81,12 @@ function getPhotoFeedBlogTotals() {
   });
 }
 
-function addPhotoFeedPosts(){
-  getPhotoFeedBlogEntries().then(data => {
-    let uniqueBlogEntries = _.unionBy(data[0], data[1], 'permlink');
-    console.log('ADDING LATEST POSTS')
-    for (var i = 0; i < uniqueBlogEntries.length; i++) {
-      db.get().db('photofeed2').collection('posts').update({entry_id: uniqueBlogEntries[i].entry_id } ,uniqueBlogEntries[i], { upsert: true }, (error, response) => {
+function storeLatestPhotoFeedPosts(){
+  getPhotoFeedBlogEntries().then(posts => {
+    for (var i = 0; i < posts.length; i++) {
+      db.get().db('photofeed2').collection('posts').update({entry_id: posts[i].entry_id } ,uniqueBlogEntries[i], { upsert: true }, (error, response) => {
         if(error || response == null) console.log(error)
       })
     }
-
   })
 }
-
-function checkForLatestFeaturedPhotographers(){
-  setInterval(addPhotoFeedPosts, INTERVAL)
-}
-
-checkForLatestFeaturedPhotographers
-
-// setTimeout(getPhotoFeedBlogTotals, 3000)
